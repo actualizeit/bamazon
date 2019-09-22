24a3ae590f438cd7ad514789defb8ccfd83f1fcd
dbell <greenadvisor@gmail.com>
2019-09-21 22:58:08 -0400

    initial code in place

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,7 +17,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    displayItems();
+    displayItems()
   });
 
 function displayItems(){
@@ -25,11 +25,51 @@ function displayItems(){
   connection.query(query, function(err, res) {
     if (err) throw err;
     for (var i = 0; i < res.length; i++) {
-      console.log("Product Name: " + res[i].product_name + 
+      console.log("ID: " + res[i].item_id + 
+                " || Product Name: " + res[i].product_name + 
                 " || Department Name: " + res[i].department_name + 
                 " || Price: " + res[i].price +
                 " || Stock Quantity: " + res[i].stock_quantity
                 );
     }
   });
+  buyItem();
 };
+function buyItem(){
+  inquirer.prompt([
+    {
+        type: "input",
+        name: "id",
+        message: "What is the ID of the item you would like to buy?",
+    },
+    {
+      type: "input",
+      name: "quantity",
+      message: "What quantity of that item would you like to buy?",
+    }
+  ]).then(function(answer) {
+    var query = "select * from products where ?"
+    connection.query(query, {item_id: answer.id}, function(err, res) {
+      if (err) throw err;
+      if (answer.quantity < res.stock_quantity){
+        console.log("Ain't anuff of it!")
+      }else{
+        stockLeft = res.stock_quantity - answer.quantity
+        console.log("That set you back $" + answer.quantity * res.price + "yo! Betta save them sheckels")
+        connection.query("UPDATE products SET ? WHERE ?",
+          [{item_id: answer.id},
+          {stock_quantity: stockLeft}], 
+          function(err) {
+            if (err) throw err;
+          }
+        )
+      }
+    })
+    displayItems()
+  })
+}
+
+// connection.query("select res.id from products where price < 3", function(err, res) {
+//   if(err) throw err;
+//   console.log(JSON.stringify(res, null, 2));
+// })
