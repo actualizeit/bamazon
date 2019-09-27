84a4832ed5ca06d5ed2b746f296d1f297954cb7c
dbell <greenadvisor@gmail.com>
2019-09-26 22:49:40 -0400

    all supervisor functions working

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -42,27 +42,61 @@ connection.connect(function(err) {
 
   function prodSales(){
     connection.query(
-        "SELECT p.department_name, SUM(p.product_sales) AS product_sales " +
-        "FROM (SELECT * FROM products GROUP BY p.department_name) p " +
-        "LEFT JOIN departments",
-        // "ON d.department_name = p.department_name",
+        "SELECT departments.department_id, departments.over_head_costs, departments.department_name, COALESCE(SUM(products.product_sales), 0) AS department_sales " +
+        "FROM products " +
+        "RIGHT JOIN departments " +
+        "ON products.department_name = departments.department_name " +
+        "GROUP BY department_id",
     function(err, res) {
         if (err) throw err;
+        for(i=0; i < res.length; i++){
+          res[i].profit = res[i].department_sales - res[i].over_head_costs;
+        }
         console.log(res);
         makeTable(res);
+        mainScreen();
     })
-
   }
 function makeTable(res){
   var Table = require('cli-table');
   var table = new Table({
-      head: ['department_id', 'department_name', 'over_head_costs', 'product_sales', 'total_profit'],
+      head: ['department_id', 'department_name', 'over_head_costs', 'department_sales', 'total_profit'],
       colWidths: [20, 20, 20, 20, 20]
   });
-//   for (var i = 0; i < res.length; i++) {
-//   table.push(
-  []
-//   ); 
-//   }
+  for (var i = 0; i < res.length; i++) {
+  table.push(
+    [res[i].department_id, res[i].department_name, res[i].over_head_costs, res[i].department_sales, res[i].profit]
+  ); 
+  }
   console.log(table.toString());
 }
+
+function createDept(){
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "deptname",
+      message: "What is the name of the department you'd like to create?",
+    },
+    {
+      type: "input",
+      name: "overhead",
+      message: "What's the overhead cost of the department?",
+    }
+]).then(function(ans) {
+  console.log(ans)
+  var query = connection.query(
+    "INSERT INTO departments SET ?",
+    {
+      department_name: ans.deptname,
+      over_head_costs: ans.overhead
+    },
+    function(err, res) {
+      if (err) throw err;
+      console.log("\n" + 
+      res.affectedRows + " department created!\n");
+      mainScreen();
+    }
+  )
+})
+};
--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ create table products(
     department_name varchar(100) null,
     price decimal(10,2) null,
     stock_quantity int(10) not null,
-    product_sales int(10) null,
+    product_sales int(10) not null default 0,
     primary key (item_id)
 );
 
@@ -19,16 +19,16 @@ create table departments(
     primary key (department_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES  ("Sponges", "I clean!", "3.50", "20"),
-		("Dog Food", "Dog Stuff", "5.00", "15"),
-        ("Brooms", "I clean!", "8.00", "8"),
-        ("Peaches", "Food", "3.00", "30"),
-        ("Cards", "Misc", "1.50", "40"),
-        ("Nerf Gun", "Awesome", "20.00", "4"),
-        ("Soap", "Personal Care", "1.00", "15"),
-        ("Candy", "Food", "0.50", "200"),
-        ("Ping Pong Balls", "Misc", "2.50", "30");
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES  ("Sponges", "I clean!", "3.50", "20", "1000"),
+		("Dog Food", "Dog Stuff", "5.00", "15", "1000"),
+        ("Brooms", "I clean!", "8.00", "8", "1000"),
+        ("Peaches", "Food", "3.00", "30", "1000"),
+        ("Cards", "Misc", "1.50", "40", "1000"),
+        ("Nerf Gun", "Awesome", "20.00", "4", "1000"),
+        ("Soap", "Personal Care", "1.00", "15", "1000"),
+        ("Candy", "Food", "0.50", "200", "1000"),
+        ("Ping Pong Balls", "Misc", "2.50", "30", "1000");
 
 
 INSERT INTO departments (department_name, over_head_costs)
